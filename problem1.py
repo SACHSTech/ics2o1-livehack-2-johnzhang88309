28a292c9ff2e683a966501cf923c6f71d2aa0dab
johnzhang88309 <john.zhang23@ycdsbk12.ca>
2021-02-23 14:50:38 +0000

    Everything

--- problem1.py
+++ problem1.py
@@ -1,1 +1,34 @@
+"""
+--------------------------------------------------------
+Name: Speed and speeding calculator.py
+Purpose:To calculate if someone is speeding given the speed limit and the recorded speed of the car 
+ 
+Author: Zhang.J
+ 
+Created: 23/02/2021
+--------------------------------------------------------
+"""
+print("***** Speed and Speeding Calculator *****")
+print("")
+
+#input statements for speed limit and recorded speed
+speed_limit = int(input("Enter the speed limit: "))
+recorded_speed = int(input("Enter the speed of your car: "))
+print("")
+
+#calculations, if statements and printing
+speed_difference = recorded_speed - speed_limit
+if speed_difference <= 0: 
+  print ("Congragulations, you are within the speed! ")
+elif speed_difference >= 1 and speed_difference <= 20:
+  print ("You are speeding and your fine is $100") 
+elif speed_difference >= 21 and speed_difference <= 30: 
+  print ("You are speeding and your fine is $270")
+else: 
+  print("You are speeding and your fine is $570")
+
+
+
+
+
 
--- problem2.py
+++ problem2.py
@@ -1,1 +1,24 @@
+"""
+--------------------------------------------------------
+Name: triangle calculator.py
+Purpose:To take the 3 sides of a shape and calculate if it is a triangle
+ 
+Author: Zhang.J
+ 
+Created: 23/02/2021
+--------------------------------------------------------
+"""
+print("***** Triangle Calculator *****")
+print ("")
 
+#input statements for side lengths
+side_1 = int(input("Enter the length of side 1: "))
+side_2 = int(input("Enter the length of side 2: "))
+side_3 = int(input("Enter the length of side 3: "))
+print("")
+
+#if statements, calculations, and print statements
+if side_1 + side_2 > side_3 and side_1 + side_3 > side_2 and side_2 + side_3 > side_1:  
+  print("It is a triangle. ")
+else:
+  print("It is not a triangle. ")
